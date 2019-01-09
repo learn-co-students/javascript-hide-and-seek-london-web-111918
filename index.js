718e83c5b0f37c901ee57d9dc37de10844a1c5d3
Andy <andypurbrick@gmail.com>
2019-01-09 19:01:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  return
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+    for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let childrenList = rankedLists[i].children;
+    for (let v = 0, c = childrenList.length; v < c; v++) {
+      childrenList[v].innerHTML = parseInt(childrenList[v].innerHTML) + n;
+    }
+  }
+}
