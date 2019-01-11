299ef1644d6df3982b6428262970c289054b9319
ja9-look <janineluk95@gmail.com>
2019-01-11 10:12:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n){
+  let rankedListEl = document.querySelectorAll('ul.ranked-list li')
+  rankedListEl = Array.from(rankedListEl)
+  rankedListEl.forEach((i) => {
+	i.innerHTML = parseInt(i.innerHTML) + n
+})
+}
+
+function deepestChild(){
+  let node = document.querySelector("#grand-node")
+  let nextNode = node.children[0]
+  while(nextNode){
+    node = nextNode;
+    nextNode = node.children[0]
+  }
+  return node;
+}
