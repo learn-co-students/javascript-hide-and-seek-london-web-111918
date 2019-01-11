a5490ed3425007503bb512e1fa8bae3cb9e633aa
jodyneckles <jody@technative.tv>
2019-01-11 09:57:26 +0000

    Done.

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ const nestedTarget = target => document.querySelector('#nested .target')
 
 
 
-// const increaseRankBy = (n) => {
-//   var lists = document.querySelectorAll('.ranked-list li')
-//   for (let i = 0; i < lists.length; i++) {
-//     debugger
-//     lists[i].innerHTML = parseInt(lists[i].innerText) + n
-//   }
-// }
 
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list');
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children;
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-    }
+const increaseRankBy = (n) => {
+  var lists = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].innerHTML = parseInt(lists[i].innerHTML) + n
   }
 }
+
+// function increaseRankBy(n) {
+//   const rankedLists = document.querySelectorAll('.ranked-list');
+//
+//   for (let i = 0, l = rankedLists.length; i < l; i++) {
+//     let children = rankedLists[i].children;
+//
+//     for (let j = 0, k = children.length; j < k; j++) {
+//       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+//     }
+//   }
+// }
 
 const deepestChild = () => {
   const allDivs = document.querySelectorAll('#grand-node div')
