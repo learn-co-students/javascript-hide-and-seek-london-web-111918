8dcdbbd4ad6616a855ffc421d4ea0dcc1056c588
Kevmsutton <kevmsutton@gmail.com>
2019-01-13 11:00:33 +0000

    Done.

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ function nestedTarget(){
   return document.querySelector('#nested').querySelector('.target')
 }
 
-function increaseRankBy(n){
-  let allLi = document.querySelectorAll('.ranked-list li')
-    // array = []
-    allLi.forEach(element => {
-      element.innerHTML = parseInt(element.innerHTML)+n
-      //array.push(parseInt(element.innerHTML)+n)
-    })
-}
+// function increaseRankBy(n){
+//   let allLi = document.querySelectorAll('.ranked-list li')
+//     // array = []
+//     allLi.forEach(element => {
+//       element.innerHTML = parseInt(element.innerHTML)+n
+//       //array.push(parseInt(element.innerHTML)+n)
+//     })
+// }
 
 function deepestChild(){
   return document.querySelector('#grand-node div div div div')
 }
 
-// function increaseRankBy(n){
-//   let rankedListEl = document.querySelectorAll('ul.ranked-list li')
-//   rankedListEl = Array.from(rankedListEl)
-//   debugger
-//   rankedListEl.forEach((i) => {
-// 	i.innerHTML = parseInt(i.innerHTML) + n
-// })
-// }
+function increaseRankBy(n){
+  let rankedListEl = document.querySelectorAll('ul.ranked-list li')
+  rankedListEl = Array.from(rankedListEl)
+  debugger
+  rankedListEl.forEach((i) => {
+	i.innerHTML = parseInt(i.innerHTML) + n
+})
+}
